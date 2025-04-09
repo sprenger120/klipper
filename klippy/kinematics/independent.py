5f89c31071e06c6a1b44bac1dafda1269414475b
Michael Albrecht <micha.albrecht95@gmail.com>
2025-04-09 16:33:39 +0200

    independent kinematics: Added init of itersolve per stepper
    Klippy was crashing due to missing call to MCU_Stepper.setup_itersolve. There is a caveat here about name of axis the stepper is allocated to and a calc_position function that may come back to bite. For now the application starts up
    Also Formatting

--- klippy/kinematics/independent.py
+++ klippy/kinematics/independent.py
@@ -10,15 +10,17 @@ from configparser import RawConfigParser
 from klippy.configfile import ConfigWrapper
 from klippy.toolhead import ToolHead
 from klippy.klippy import Printer
-from klippy.stepper import PrinterStepper, MCU_stepper
+from klippy.stepper import PrinterStepper, MCU_stepper, error
+from klippy.gcode import Coord
+
 
 class IndependentKinematics:
-    def __init__(self, toolhead : ToolHead, config : ConfigWrapper):
-        self._printer : Printer = config.get_printer()
-        self._toolhead : ToolHead = toolhead
-        self._raw_config : RawConfigParser = config.fileconfig
+    def __init__(self, toolhead: ToolHead, config: ConfigWrapper):
+        self._printer: Printer = config.get_printer()
+        self._toolhead: ToolHead = toolhead
+        self._raw_config: RawConfigParser = config.fileconfig
 
-        self._steppers : [Dict[str, MCU_stepper]] = []
+        self._steppers: [Dict[str, MCU_stepper]] = []
 
         # load MCU_Stepper instances
         # We are not bound by XYZ and have to look for config section names starting with "stepper_.."
@@ -27,30 +29,51 @@ class IndependentKinematics:
         for section in self._raw_config.sections():
             DELIMITER = "_"
             if section.startswith('stepper' + DELIMITER):
-                axis_name : str = section.split(DELIMITER)[1]
-                # PrinterStepper is a helper function to create a MCU_Stepper object
-                inst = PrinterStepper(config.getsection(section))
-                self._steppers.append({axis_name : inst})
+                axis_name: str = section.split(DELIMITER)[1]
+                inst = self._create_and_setup_mcu_stepper_inst(config.getsection(section))
+                if inst is None:
+                    raise error("Creation of MCU_Stepper instance failed")
+                self._steppers.append({axis_name: inst})
 
-        self.axes_minmax = toolhead.Coord(0., 0., 0., 0.)
+        # toolhead.Coord is fixed to X,Y,Z,E coordinates
+        # Leaving it for now until a solution that satisfies our dynamic amount of steppers is found
+        self.axes_minmax: Coord = toolhead.Coord(0., 0., 0., 0.)
+
     def get_steppers(self) -> [MCU_stepper]:
         return [stepper for axis_name, stepper in self._steppers]
+
     def calc_position(self, stepper_positions):
         return [0, 0, 0]
+
     def set_position(self, newpos, homing_axes):
         pass
+
     def clear_homing_state(self, clear_axes):
         pass
+
     def home(self, homing_state):
         pass
+
     def check_move(self, move):
         pass
+
     def get_status(self, eventtime):
         return {
             'homed_axes': '',
             'axis_minimum': self.axes_minmax,
             'axis_maximum': self.axes_minmax,
         }
+
+    def _create_and_setup_mcu_stepper_inst(self, stepper_section_config: ConfigWrapper) -> MCU_stepper | None:
+        # PrinterStepper is a helper function to create an MCU_Stepper object
+        inst: MCU_stepper = PrinterStepper(stepper_section_config)
+
+        # cartesian_stepper_alloc will set a calc_position callback internally depending on axis name
+        # it has callbacks for X,Y,Z and nothing else. Not having a callback may come back to bite.
+        fake_axis = "0"
+        inst.setup_itersolve('cartesian_stepper_alloc', fake_axis.encode())
+        return inst
+
 
 def load_kinematics(toolhead, config):
     return IndependentKinematics(toolhead, config)
