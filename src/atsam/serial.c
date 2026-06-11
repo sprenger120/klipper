bd53fa5807aad971186f8a5d642bd71de06849db
Michael Albrecht <micha.albrecht95@gmail.com>
2026-06-11 17:07:27 +0200

    atsam serial driver: Moved logic to own file, reverted original serial code
    - Half-duplex requires USART peripheral,
    original code only uses UART
    - Renamed "RS485" references to Half-Duplex as it fits better

--- src/atsam/serial.c
+++ src/atsam/serial.c
@@ -10,7 +10,6 @@
 #include "command.h" // DECL_CONSTANT_STR
 #include "internal.h" // gpio_peripheral
 #include "sched.h" // DECL_INIT
-#include "gpio.h" // gpio_out_setup
 
 // Serial port pins
 #if CONFIG_MACH_SAM3X
@@ -34,14 +33,6 @@ static const uint32_t Pmc_id = ID_UART0;
 static const uint32_t rx_pin = GPIO('A', 9), tx_pin = GPIO('A', 10);
 static const char uart_periph = 'A';
 DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA9,PA10");
-#elif CONFIG_ATSAM_RS_485_SERIAL && CONFIG_MACH_SAME70
-#define UARTx_IRQn UART2_IRQn
-static Uart * const Port = UART2;
-static const uint32_t Pmc_id = ID_UART2;
-static const uint32_t rx_pin = GPIO('D', 15), tx_pin = GPIO('D', 16);
-static const uint32_t drive_enable_pin = GPIO('C', 10);
-static const char uart_periph = 'B';
-DECL_CONSTANT_STR("RESERVE_PINS_serial", "PD15,PD16,PC10");
 #elif CONFIG_MACH_SAME70
 #define UARTx_IRQn UART2_IRQn
 static Uart * const Port = UART2;
@@ -51,30 +42,9 @@ static const char uart_periph = 'C';
 DECL_CONSTANT_STR("RESERVE_PINS_serial", "PD25,PD26");
 #endif
 
-#if CONFIG_ATSAM_RS_485_SERIAL
-struct gpio_out drive_enable_out = {0};
-uint8_t first_serial_data_byte = {};
-
-#define STEP_WAIT_IDLE 0
-#define STEP_WAIT_BREAK 1
-#define STEP_TX_UNTIL_EMPTY 2
-#define STEP_FINISHED 3
-uint8_t step = STEP_FINISHED;
-#endif
-
-// TX irq enabled -> check for data in queue
-// - no data -> disable
-// - data -> enable idle line detection, disable tx irq (wait for empty line until send)
-// - Idle line IRQ received -> disable idle line, send break, enable tx irq
-// - wait for tx irq, disable break, transmit until empty
-// - disable tx irq
-
 void
 UARTx_Handler(void)
 {
-    // tx interrupt can be activated any time!
-
-    
     uint32_t status = Port->UART_SR;
     if (status & UART_SR_RXRDY)
         serial_rx_byte(Port->UART_RHR);
@@ -82,14 +52,9 @@ UARTx_Handler(void)
         uint8_t data;
         int ret = serial_get_tx_byte(&data);
         if (ret)
-        {
-            gpio_out_write(drive_enable_out, 0);
             Port->UART_IDR = UART_IDR_TXRDY;
-        } else
-        {
-            gpio_out_write(drive_enable_out, 1);
+        else
             Port->UART_THR = data;
-        }
     }
 }
 
@@ -104,7 +69,6 @@ serial_init(void)
 {
     gpio_peripheral(rx_pin, uart_periph, 1);
     gpio_peripheral(tx_pin, uart_periph, 0);
-    drive_enable_out = gpio_out_setup(drive_enable_pin, 0);
 
     // Reset uart
     enable_pclock(Pmc_id);
---
+++ src/atsam/serial_halfduplex.c
@@ -0,0 +1,114 @@
+// same70 half-duplex serial port
+//
+// Copyright (C) 2026  Michael Albrecht <michael.albrecht@ovgu.de>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include "autoconf.h" // CONFIG_SERIAL_BAUD
+#include "board/armcm_boot.h" // armcm_enable_irq
+#include "board/serial_irq.h" // serial_rx_data
+#include "command.h" // DECL_CONSTANT_STR
+#include "internal.h" // gpio_peripheral
+#include "sched.h" // DECL_INIT
+#include "gpio.h" // gpio_out_setup
+
+#ifdef CONFIG_MACH_SAME70
+#define USARTx_IRQn USART2_IRQn
+static Usart * const Port = USART2;
+static const uint32_t Pmc_id = ID_USART2;
+static const uint32_t rx_pin = GPIO('D', 15), tx_pin = GPIO('D', 16);
+static const uint32_t drive_enable_pin = GPIO('C', 10);
+static const char usart_periph = 'B';
+DECL_CONSTANT_STR("RESERVE_PINS_serial", "PD15,PD16,PC10");
+#else
+#error "Architecture not supported"
+#endif
+
+
+struct gpio_out drive_enable_out = {0};
+uint8_t first_serial_data_byte = {};
+
+#define STEP_WAIT_IDLE 0
+#define STEP_WAIT_BREAK 1
+#define STEP_TX_UNTIL_EMPTY 2
+#define STEP_FINISHED 3
+uint8_t step = STEP_FINISHED;
+
+
+// TX irq enabled -> check for data in queue
+// - no data -> disable
+// - data -> enable idle line detection, disable tx irq (wait for empty line until send)
+// - Idle line IRQ received -> disable idle line, send break, enable tx irq
+// - wait for tx irq, disable break, transmit until empty
+// - disable tx irq
+
+void
+serial_enable_tx_irq(void)
+{
+    Port->US_IER = US_IER_TXRDY;
+}
+
+void
+serial_disable_tx_irq(void)
+{
+    Port->US_IDR = US_IDR_TXRDY;
+}
+
+void serial_start_break(void)
+{
+    // Starts transmission of a break after the characters present in US_THR and the Transmit Shift Register have
+    // been transmitted. No effect if a break is already being transmitted.
+    Port->US_CR = US_CR_USART_STTBRK_Msk;
+}
+void serial_stop_break(void)
+{
+    // Stops transmission of the break after a minimum of one character length and transmits a high level during
+    // 12-bit periods. No effect if no break is being transmitted.
+    Port->US_CR = US_CR_USART_STPBRK_Msk;
+}
+
+// todo timeout
+
+void
+USARTx_Handler(void)
+{
+    uint32_t status = Port->US_CSR;
+    if (status & US_CSR_RXRDY_Msk)
+    {
+        serial_rx_byte(Port->US_RHR);
+    }
+    if (status & US_CSR_TXRDY_Msk) {
+        uint8_t data;
+        // todo drive pin
+        int ret = serial_get_tx_byte(&data);
+        if (ret)
+            serial_disable_tx_irq();
+        else
+            Port->US_THR = data;
+        // drive pin disable on TXEMPTY
+    }
+}
+
+
+void
+serial_halfduplex_init(void)
+{
+    gpio_peripheral(rx_pin, usart_periph, 1);
+    gpio_peripheral(tx_pin, usart_periph, 0);
+    drive_enable_out = gpio_out_setup(drive_enable_pin, 0);
+
+    // Reset uart
+    enable_pclock(Pmc_id);
+    Port->US_CR = (US_CR_RSTRX | US_CR_RSTTX
+                     | US_CR_RXDIS | US_CR_TXDIS);
+    Port->US_IDR = 0xFFFFFFFF;
+
+    // Enable uart
+    Port->US_MR = (US_MR_USART_MODE_NORMAL | US_MR_USART_CHMODE_NORMAL | US_MR_USCLKS_MCK |
+        US_MR_CHRL_8_BIT | US_MR_USART_PAR_NO);
+    Port->US_BRGR = get_pclock_frequency(Pmc_id) / (16 * CONFIG_SERIAL_BAUD);
+    Port->US_IER = UART_IER_RXRDY;
+    armcm_enable_irq(USARTx_Handler, USARTx_IRQn, 0);
+    Port->US_CR = US_CR_RXEN | US_CR_TXEN;
+}
+DECL_INIT(serial_halfduplex_init);
