86bc657d4536ec44bafc6acbf031894057ce7145
Michael Albrecht <micha.albrecht95@gmail.com>
2026-06-19 20:53:18 +0200

    atsam halfduplex serial: Finished implementation
    - used a state machine to manage
    complexity

--- src/atsam/serial_halfduplex.c
+++ src/atsam/serial_halfduplex.c
@@ -11,6 +11,7 @@
 #include "internal.h" // gpio_peripheral
 #include "sched.h" // DECL_INIT
 #include "gpio.h" // gpio_out_setup
+#include "board/io.h"
 
 #ifdef CONFIG_MACH_SAME70
 #define USARTx_IRQn USART2_IRQn
@@ -28,19 +29,14 @@ DECL_CONSTANT_STR("RESERVE_PINS_serial", "PD15,PD16,PC10");
 struct gpio_out drive_enable_out = {0};
 uint8_t first_serial_data_byte = {};
 
-#define STEP_WAIT_IDLE 0
-#define STEP_WAIT_BREAK 1
-#define STEP_TX_UNTIL_EMPTY 2
-#define STEP_FINISHED 3
-uint8_t step = STEP_FINISHED;
+#define STATE_IDLE 0
+#define STATE_INITIAL_TXRDY 1
+#define STATE_IDLE_LINE_TIMEOUT 2
+#define STATE_BREAK_FINSHED_TXRDY 3
+#define STATE_TX_UNTIL_QUEUE_EMPTY_TXRDY 4
+#define STATE_TX_UNTIL_EMPTY_TXEMPTY 5
+uint8_t state = STATE_IDLE;
 
-
-// TX irq enabled -> check for data in queue
-// - no data -> disable
-// - data -> enable idle line detection, disable tx irq (wait for empty line until send)
-// - Idle line IRQ received -> disable idle line, send break, enable tx irq
-// - wait for tx irq, disable break, transmit until empty
-// - disable tx irq
 
 void
 serial_enable_tx_irq(void)
@@ -54,7 +50,21 @@ serial_disable_tx_irq(void)
     Port->US_IDR = US_IDR_TXRDY;
 }
 
-// after calling this function, wait for TXRDY and then call stop break
+void
+serial_data_direction_tx(void)
+{
+    //Port->US_CR = US_CR_RXDIS | US_CR_TXEN;
+    gpio_out_write(drive_enable_out, 1);
+}
+
+
+void
+serial_data_direction_rx(void)
+{
+    //Port->US_CR = US_CR_RXEN | US_CR_TXDIS;
+    gpio_out_write(drive_enable_out, 0);
+}
+
 void
 serial_start_break(void)
 {
@@ -94,27 +104,92 @@ serial_end_timeout(void)
 }
 
 
+
 void
 USARTx_Handler(void)
 {
+    uint8_t data;
     uint32_t status = Port->US_CSR;
     if (status & US_CSR_RXRDY_Msk)
     {
         serial_rx_byte(Port->US_RHR);
+        return;
     }
     if (status & US_CSR_TXRDY_Msk) {
-        uint8_t data;
-        // todo drive pin
-        int ret = serial_get_tx_byte(&data);
-        if (ret)
+        // applications signals available TX data by enabling TXRDY IRQ
+        // -> TX can't be disabled to avoid getting into this branch
+        //     to never miss chance to TX.
+        // This switch-case has to have all available states to not land in
+        // default case even when current state doesn't care about TXRDY.
+        // TXRDY is spurious in nature.
+        switch (readb(&state))
+        {
+        case STATE_IDLE:
             serial_disable_tx_irq();
-        else
+            if (serial_get_tx_byte(&first_serial_data_byte))
+            {
+                // no data available
+                break;
+            }
+            writeb(&state, STATE_INITIAL_TXRDY);
+            serial_data_direction_rx();
+            serial_start_timeout();
+            break;
+        case STATE_INITIAL_TXRDY:
+            // avoid default case due to spurious TXRDY nature
+            break;
+        case STATE_IDLE_LINE_TIMEOUT:
+            serial_stop_break();
+            serial_data_direction_tx();
+            Port->US_THR = first_serial_data_byte;
+            serial_enable_tx_irq();
+            writeb(&state, STATE_BREAK_FINSHED_TXRDY);
+            break;
+        case STATE_BREAK_FINSHED_TXRDY:
+            [[fallthrough]];
+        case STATE_TX_UNTIL_QUEUE_EMPTY_TXRDY:
+            writeb(&state, STATE_TX_UNTIL_QUEUE_EMPTY_TXRDY);
+            if (serial_get_tx_byte(&data))
+            {
+                // TX queue emptied
+                // wait for peripheral to signal it's completely finished
+
+                serial_disable_tx_irq();
+                // enable TXEMPTY irq
+                Port->US_IER = US_IER_TXEMPTY_Msk;
+                writeb(&state, STATE_TX_UNTIL_EMPTY_TXEMPTY);
+                break;
+            }
             Port->US_THR = data;
-        // drive pin disable on TXEMPTY
+            break;
+        case STATE_TX_UNTIL_EMPTY_TXEMPTY:
+            // avoid default case due to spurious TXRDY nature
+            break;
+        default:
+            shutdown("Invalid state");
+        }
+    }
+    if (status & US_CSR_TXEMPTY_Msk)
+    {
+        // disable TXEMPTY irq
+        Port->US_IDR = US_IDR_TXEMPTY_Msk;
+        if (readb(&state) == STATE_TX_UNTIL_EMPTY_TXEMPTY)
+        {
+            serial_data_direction_rx();
+            writeb(&state, STATE_IDLE);
+        }
     }
     if (status & US_CSR_USART_LIN_TIMEOUT_Msk)
     {
-        // todo
+        serial_end_timeout();
+        if (readb(&state) == STATE_INITIAL_TXRDY)
+        {
+            volatile int a = 0;
+            serial_data_direction_tx();
+            serial_start_break();
+            serial_enable_tx_irq();
+            writeb(&state, STATE_IDLE_LINE_TIMEOUT);
+        }
     }
 }
 
@@ -136,8 +211,10 @@ serial_halfduplex_init(void)
     Port->US_MR = (US_MR_USART_MODE_NORMAL | US_MR_USART_CHMODE_NORMAL | US_MR_USCLKS_MCK |
         US_MR_CHRL_8_BIT | US_MR_USART_PAR_NO);
     Port->US_BRGR = get_pclock_frequency(Pmc_id) / (16 * CONFIG_SERIAL_BAUD);
+
+    serial_data_direction_rx();
+    Port->US_CR = US_CR_RXEN_Msk | US_CR_TXEN_Msk;
     Port->US_IER = US_IER_RXRDY_Msk;
     armcm_enable_irq(USARTx_Handler, USARTx_IRQn, 0);
-    Port->US_CR = US_CR_RXEN | US_CR_TXEN;
 }
 DECL_INIT(serial_halfduplex_init);
